538dfcf6094d560601f9ad8df3dabcbe6bca62d9
JustCollabs <admin@justcollabs.com>
2025-05-27 15:48:03 +0800

    use want2c user

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,14 +10,14 @@ const config: Config = {
   favicon: 'img/logo.png',
 
   // Set the production url of your site here
-  url: 'https://ericpb.github.io',
+  url: 'https://want2c.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/justcollabs-faq/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ericpb', // Usually your GitHub org/user name.
+  organizationName: 'want2c', // Usually your GitHub org/user name.
   projectName: 'justcollabs-faq', // Usually your repo name.
 
   onBrokenLinks: 'throw',
